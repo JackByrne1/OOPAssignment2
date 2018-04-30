55e10a02b629570b9ceb39d9d57397ab9a0a78f9
Josh Lambert <joshl085@hotmail.com>
2018-04-30 15:16:30 +0100

    commit

--- Unity/FPS/Assets/Gun.cs
+++ Unity/FPS/Assets/Gun.cs
@@ -2,47 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour {
+public class Gun : MonoBehaviour
+{
 
-	public GameObject weapon01;
-	public GameObject weapon02;
-
-
-	public float damage = 10f;
-	public float range = 100f;
-	public float impactForce = 50f;
-	public float score = 0;
-
-	public Camera fpsCam;
-	// public ParticleSystem muzzleFlash;
-	// public GameObject impactEffect;
+    public GameObject weapon01;
+    public GameObject weapon02;
 
 
-	public GameObject bulletSpawnPoint;
-	public GameObject bulletPrefab;
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 50f;
+    public float score = 0;
 
-	void Start()
-	{
-		weapon01.SetActive(true);
-		weapon02.SetActive(false);
-	}
-
-	void Update()
-	{
-		if(Input.GetKeyDown("1"))
-		{
-			weapon01.SetActive(true);
-			weapon02.SetActive(false);
-		}
-
-		if (Input.GetKeyDown("2"))
-		{
-			weapon01.SetActive(false);
-			weapon02.SetActive(true);
-		}
+    public Camera fpsCam;
+    // public ParticleSystem muzzleFlash;
+    // public GameObject impactEffect;
 
 
-		/*  if (Input.GetButtonDown("Fire1"))
+    public GameObject bulletSpawnPoint;
+    public GameObject bulletPrefab;
+
+    void Start()
+    {
+        weapon01.SetActive(true);
+        weapon02.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            weapon01.SetActive(true);
+            weapon02.SetActive(false);
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            weapon01.SetActive(false);
+            weapon02.SetActive(true);
+        }
+
+
+        /*  if (Input.GetButtonDown("Fire1"))
           {
 
               GameObject bullet = GameObject.Instantiate<GameObject>(bulletPrefab);
@@ -54,48 +55,48 @@ public class Gun : MonoBehaviour {
           }*/
 
 
-		if (Input.GetButtonDown("Fire1"))
-		{
-			Shoot();
-		}
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
 
-	}
-
-
-	void Shoot()
-	{
-		//muzzleFlash.Play(); 
-
-		RaycastHit hit;
-		if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
-		{
-			Debug.Log(hit.transform.name);
-
-			Target target = hit.transform.GetComponent<Target>();
-			if (target != null)
-			{
-				target.TakeDamage(damage);
-
-			}
-
-			if (hit.rigidbody != null)
-			{
-				hit.rigidbody.AddForce(-hit.normal * impactForce);
-				score = score + 5;
-			}
-
-			//  GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-			//Destroy(impactGO, 2f);
-		}
+    }
 
 
+    void Shoot()
+    {
+        //muzzleFlash.Play(); 
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+                score = score + 5;
+            }
+
+            //  GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            //Destroy(impactGO, 2f);
+        }
 
 
-	}
 
 
-	// Update is called once per frame
-	/*void Update ()
+    }
+
+
+    // Update is called once per frame
+    /*void Update ()
     {
 
         if(Input.GetButtonDown("Fire1"))
--- Unity/FPS/Assets/Target.cs
+++ Unity/FPS/Assets/Target.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 
-public class Target : MonoBehaviour {
+public class Target : MonoBehaviour
+{
 
-    public float health = 50f;
+    public float health = 20f;
+
 
     public void TakeDamage(float amount)
     {
         health -= amount;
         if (health <= 0f)
         {
+
             Die();
+
         }
     }
 
