bf7f244f4e65fa7d6aefee497da7a17a27dbc345
Josh Lambert <joshl085@hotmail.com>
2018-04-30 23:20:19 +0100

    final1

---
+++ Unity/FPS/Assets/LevelsMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public float timeRemaining;
+
+	public void PlayGame ()
+	{
+        timeRemaining = 15;
+		SceneManager.LoadScene ("prac");
+	}
+
+	public void PlayGame2 ()
+	{
+        timeRemaining = 10;
+
+        SceneManager.LoadScene ("prac2");
+	}
+
+	public void PlayGame3 ()
+	{
+        timeRemaining = 5;
+
+        SceneManager.LoadScene ("prac3");
+	}
+}
+
