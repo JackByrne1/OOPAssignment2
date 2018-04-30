bafffcb826da1addb58e0ff9ca18bfaf894cbded
Josh Lambert <joshl085@hotmail.com>
2018-04-30 10:25:34 +0100

    test

--- Unity/FPS/Assets/DestroyCubes.cs
+++ Unity/FPS/Assets/DestroyCubes.cs
@@ -1,19 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class DestroyCubes : MonoBehaviour {
-    
 
+    public int count = 0;
+    public Text countText;
+
+   
     private void OnCollisionEnter(Collision bulletPrefab)
     {
 
         if (bulletPrefab.gameObject.name == "Cube")
         {
+            
             Destroy(bulletPrefab.gameObject);
-    
+            count= count+ 1;
+            SetCountText();
+
         }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
 
     }
 
